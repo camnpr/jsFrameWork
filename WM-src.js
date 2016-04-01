b5553e82a525fb4390a89745441350ffa303fdc7
camnpr <camnpr@163.com>
2016-04-01 10:57:57 +0800

    Update WM-src.js

--- WM-src.js
+++ WM-src.js
@@ -129,8 +129,186 @@
 		/**
 		*@description 空图片URL
 		*/
-		emptyImageUrl: 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs='
+		emptyImageUrl: 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=',
 
+       /**
+        *@description 设置cookie
+        */
+        setCookie:function(sName,sValue,oExpires,sPath,sDomain,bSecure){
+            var currDate = new Date(),
+                sExpires = typeof oExpires == 'undefined'?'':';expires=' + new Date(currDate.getTime() + (oExpires * 24 * 60 * 60* 1000)).toUTCString();
+            document.cookie = sName + '=' + sValue + sExpires + ((sPath == null)?'':(' ;path=' + sPath)) + ((sDomain == null)?'':(' ;domain=' + sDomain)) + ((bSecure == true)?' ; secure':'');
+        },
+
+         /**
+        *@description 获取cookie
+        */
+        getCookie:function(sName){
+            var regRes = document.cookie.match(new RegExp("(^| )" + sName + "=([^;]*)(;|$)"));
+            return (regRes != null)?unescape(regRes[2]):'-';
+        },
+
+        /**
+        *@description base64加密
+        */
+        base64encode: function(G) {
+                var A = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+                var C, E, z;
+                var F, D, B;
+                z = G.length;
+                E = 0;
+                C = "";
+                while (E < z) {
+                    F = G.charCodeAt(E++) & 255;
+                    if (E == z) {
+                        C += A.charAt(F >> 2);
+                        C += A.charAt((F & 3) << 4);
+                        C += "==";
+                        break
+                    }
+                    D = G.charCodeAt(E++);
+                    if (E == z) {
+                        C += A.charAt(F >> 2);
+                        C += A.charAt(((F & 3) << 4) | ((D & 240) >> 4));
+                        C += A.charAt((D & 15) << 2);
+                        C += "=";
+                        break
+                    }
+                    B = G.charCodeAt(E++);
+                    C += A.charAt(F >> 2);
+                    C += A.charAt(((F & 3) << 4) | ((D & 240) >> 4));
+                    C += A.charAt(((D & 15) << 2) | ((B & 192) >> 6));
+                    C += A.charAt(B & 63)
+                }
+                return C
+            },
+            /**
+            *@description 全球唯一标识符
+            */
+            guid: function(){
+                    var e = (new Date).getTime();
+                    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(t) {
+                                var r, n;
+                                return r = (e + 16 * Math.random()) % 16 | 0,
+                                e = Math.floor(e / 16),
+                                n = "x" === t ? r : 3 & r | 8,
+                                n.toString(16)
+                            });
+            },
+            addEvent: function(o,s,fn){
+                o.attachEvent?o.attachEvent('on'+s,fn):o.addEventListener(s,fn,false);
+                return o;                
+            },
+            domReady: function(domLoadedHandler){
+                    /*if (readyBound ) return;
+                    readyBound = true;*/
+
+                    if ( document.addEventListener ) {
+                        document.addEventListener( "DOMContentLoaded", function(){
+                            document.removeEventListener( "DOMContentLoaded", arguments.callee, false );
+                            domLoadedHandler;
+                        }, false );
+
+                    } else if ( document.attachEvent ) {
+                        // 考虑网页中嵌入iframes
+                        document.attachEvent("onreadystatechange", function(){
+                            if ( document.readyState === "complete" ) {
+                                document.detachEvent( "onreadystatechange", arguments.callee );
+                                domLoadedHandler;
+                            }
+                        });
+
+                        //是IE并且没有iframe，不断检查，看看是否准备好
+                        if ( document.documentElement.doScroll && typeof window.frameElement === "undefined" ) (function(){
+                            //if ( jQuery.isReady ) return;
+                            try {
+                                //如果是IE，使用这个技巧 http://javascript.nwbox.com/IEContentLoaded/
+                                document.documentElement.doScroll("left");
+                            } catch( error ) {
+                                setTimeout( arguments.callee, 0 );
+                                return;
+                            }
+
+                            //执行等待的函数
+                            domLoadedHandler;
+                        })();
+                    }
+
+                    window.addEventListener('load',domLoadedHandler,false);
+              },
+              beforeunload:function(){ // 兼容卸载页面代码，统计页面停留时间。 （为了防止页面beforeunload不执行，采用定时发起请求）
+
+              },
 	};
+
+        /**
+    *@description Ajax 兼容IE6+
+    */  
+    ST.Ajax = {
+        config:{
+            contentType: 'application/x-www-form-urlencoded'
+        },
+        parse: function (req) {
+            var result;
+            try {
+                //var a = !!window.ActiveXObject && +/msie\s(\d+)/i.exec(navigator.userAgent)[1] || 0 / 0;
+                // 9 > a ? 加载json2 : 
+                  result = JSON.parse(req.responseText);
+                } catch (e) {
+                  result = req.responseText;
+                }
+            return [result, req];
+        },
+        xhr: function (type, url, data) {
+                var methods = {
+                  success: function () {},
+                  error: function () {},
+                  always: function () {}
+                }, parse =  this.parse;
+                var XHR = XMLHttpRequest || ActiveXObject;
+                var request = new XHR('MSXML2.XMLHTTP.3.0');
+
+                request.open(type, url, true);
+                request.setRequestHeader('Content-type', this.config.contentType);
+                request.onreadystatechange = function () {
+                  var req;
+                  if (request.readyState === 4) {
+                    req = parse(request);
+                    if (request.status >= 200 && request.status < 300) {
+                      methods.success.apply(methods, req);
+                    } else {
+                      methods.error.apply(methods, req);
+                    }
+                    methods.always.apply(methods, req);
+                  }
+                };
+                request.send(data);
+
+                var atomXHR = {
+                  success: function (callback) {
+                    methods.success = callback;
+                    return atomXHR;
+                  },
+                  error: function (callback) {
+                    methods.error = callback;
+                    return atomXHR;
+                  },
+                  always: function (callback) {
+                    methods.always = callback;
+                    return atomXHR;
+                  }
+                };
+                return atomXHR;
+           },
+          get: function (src) {
+            return this.xhr('GET', src);
+          },
+          post: function (url, data) {
+            return this.xhr('POST', url, data);
+          },
+          setContentType: function(value) {
+            this.config.contentType = value;
+          }
+    };
 	
 }(window, document));
